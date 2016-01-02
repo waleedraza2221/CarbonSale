9f279e5a2fab025c89d6dd5b685a48e31b33f365
waleedraza2221 <waleedraza221@gmail.com>
2016-01-02 20:32:51 +0500

    Starting

---
+++ CarbonSale/CarbonSale/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CarbonSale.MvcApplication" Language="C#" %>
---
+++ CarbonSale/CarbonSale/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CarbonSale.Startup))]
+namespace CarbonSale
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
